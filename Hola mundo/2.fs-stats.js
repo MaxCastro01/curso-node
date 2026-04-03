1e4a3ea49a5d39d81d9ed404721d85d05de61773
Max Castro <max.chdz00@gmail.com>
2026-04-03 13:34:25 -0700

    Repaso

--- Hola mundo/2.fs-stats.js
+++ Hola mundo/2.fs-stats.js
@@ -1,10 +1,16 @@
 const fs = require('node:fs')
 
+// creamos una variable que contendra nuestro archivo a leer
 const stats = fs.statSync('./archivo.txt')
 
+//su estructura consta de un const debido a que no cambiara
+//Su nombre Stats
+//se inicia su valor con fs, dado que usaremos este modulo para acceder a el
+//statsSync, esta es una funcion del modulo para poder acceder a las estadisticas del archivo
+
 console.log(
-    stats.isFile(),
-    stats.size,
-    stats.isDirectory(),
+    stats.isFile(), //es un directorio
+    stats.size, //tamaño del archivo, porque no es una funcion, debido a que es una propiedad directa del archivo
+    stats.isDirectory(), //es un directorio
     stats.isSymbolicLink() //un link simbolico es un archivo que contiene la direccion a otro archivo
 );
--- Hola mundo/3.fs-read.js
+++ Hola mundo/3.fs-read.js
@@ -1,8 +1,12 @@
+//Importamos la herramienta de fs para poder leer archivos
 const fs = require('node:fs');
 
 
-
+//primer mensaje en consola
 console.log('Leyendo el primer archivo...');
+
+//leemos el archivo
+
 fs.readFile('./archivo.txt','utf-8', (err,text) =>{
     console.log(text);
 })
