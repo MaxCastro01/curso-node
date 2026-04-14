1170e4b24250143ebbc977e24b4e854b189e15df
Max Castro <max.chdz00@gmail.com>
2026-04-13 22:33:04 -0700

    Avance de pagina login

--- Pagina-Login-Register/app/index.js
+++ Pagina-Login-Register/app/index.js
@@ -10,7 +10,9 @@ import { fileURLToPath } from "url";
 // import.meta.url nos da la ruta del archivo pero en formato URL
 // fileURLToPath(import.meta.url) convertimos esa URL a una ruta del sistema
 // path.dirname obtiene la carpeta, no el archivo
-// path
+// Que es path, da herramientas para poder manejar rutas y directorios 
+// en el sistema, de forma independiente del sistema operativo
+// 
 // app.get("/",(req, res) => res.sendFile(__dirname + "/pages/login.html"))
 // Lo que hace esto es cuando alguien entra a nuestro servidor o pagina de inicio
 // nuestro servidor va a responder mandandonlo a la pagina de login.html
@@ -42,7 +44,7 @@ console.log("Servidor corriendo en puerto",app.get("port"));
 // entonces no lo cuentro porque busca dentro de la carpeta del html
 // con "..", hacemos que retroceda y salga de esa carpeta para buscar 
 // la que indicamos que en este caso es public
-app.use(express.static(path.join(__dirname,"public")))
+
 app.use(express.static(path.join(__dirname,"public")))
 
 
@@ -52,7 +54,7 @@ app.use(express.static(path.join(__dirname,"public")))
 //Donde si alguien entra a esa pagina manda la solicitud
 //nuestro servidor tiene que responder con algo
 //app que es nuestro servidor.get toma
-// /la direccion
+// "/" indicamos que esto seria como la pagina de inicio de nuestra pagina
 // Tendra dos parametros que sera req, solicitud y res respuesta
 // Hara sera responder, mandando el archivo de nuestro login.html
 // para poder acceder a nuestra ruta donde esta el directorio
@@ -60,10 +62,13 @@ app.use(express.static(path.join(__dirname,"public")))
 //usaremos mejor path.join que es mejor para legilibilidad
 // en vez de usar concatenacion de string y tener que poner /
 // usaremos path.join para indicar el nombre de la carpeta y del archivo
+
 app.get("/",(req, res) => res.sendFile(path.join(__dirname,"pages","index.html")))
+
 // cuando usamos / es para indicar la pagina de inicio
 // si queremos mandar a otra pagina hija por asi decirlo
 // usamos el /"nombre de la pagina" EJEMPLO http://localhost:4000/register
+
 app.get("/login", (req,res) => res.sendFile(path.join(__dirname,"pages","login.html")))
 app.get("/register",(req, res) => res.sendFile(path.join(__dirname,"pages","register.html")))
 
